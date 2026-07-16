cfee095e8eb7cce9037fb7a57d0ac516fe5ed154
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-07-16 12:24:56 +0500

    feat(registerUser) for now just follow simple steps -take data fron frontend -check in the DB is user already exists -if not then simply create and SAVE the user next need to generate Acc/Ref token i'll do soon

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import {User} from "../models/user.models.js";
+import ApiResponse from "../utils/api-response.js";
+import ApiError from "../utils/api-error.js";
+import asyncHandler from "../utils/async-handler.js";
+
+
+const registerUser = asyncHandler(async(req,res)=>{
+    const {username,email,password,role} = req.body
+
+    const existingUser = await User.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    });
+
+    if(existingUser){
+        throw new ApiError(409,"User with email or username already exists",[])
+    }
+
+    const user = User.create({ //here User-->not able to access schema methods because it's the mongoose model but {user} can why? bcz User is a mongoose method we already imported from model itsel
+        username,
+        email,
+        password,
+        isEmailVerified: false
+    })
+    // because our use have User(schema) So it have all the method available in the userSchema
+})
+
