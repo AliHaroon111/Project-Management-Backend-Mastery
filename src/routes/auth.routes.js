c26a4c4799f0b7d14ee17518fd03b487e1a28397
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-07-19 10:34:46 +0500

    feat: Added the userLogin Validator - what validator do it fetch errors from data - and keep them in req - the this req is used by validationResult(req) to fetch errors - write and put the validation in routes for loginUser

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express"
 import { login, registerUser } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js"
-import { userRegisterValidator } from "../validators/index.js"
+import { userLoginValidator, userRegisterValidator } from "../validators/index.js"
 
 const router = Router();
 
 // userRegisterValidator() ---> this will process the errors -- validate collects error and complete the proccess
 router.route("/register").post(userRegisterValidator(), validate, registerUser)
-router.route("/login").post(login)
+router.route("/login").post(userLoginValidator(), validate, login)
 
 export default router
