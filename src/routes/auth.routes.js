4fae40473cb95b2bcd01ff42a73fae5580b0b625
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-07-21 10:08:52 +0500

    feat(logout): add logout user controller - first of all we have a refreshToken in our user models - we can only logout if the user already loggedIn - so we use middleware in between that verifyJWT --> verifyJWT give us the {req.user}

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express"
-import { login, registerUser } from "../controllers/auth.controller.js";
+import { login, logoutUser, registerUser } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js"
-import { userLoginValidator, userRegisterValidator } from "../validators/index.js"
+import { 
+    userLoginValidator, 
+    userRegisterValidator
+ } from "../validators/index.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // userRegisterValidator() ---> this will process the errors -- validate collects error and complete the proccess
 router.route("/register").post(userRegisterValidator(), validate, registerUser)
 router.route("/login").post(userLoginValidator(), validate, login)
+
+// secure routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router
