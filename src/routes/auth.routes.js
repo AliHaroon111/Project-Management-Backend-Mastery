91eae94e263d89c2ecdd148ce2f0e01f8e361007
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-07-24 19:06:57 +0500

    feat(routes): implement auth and user management endpoints into the routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,20 +1,31 @@
 import { Router } from "express"
-import { login, logoutUser, registerUser, getCurrentUser } from "../controllers/auth.controller.js";
+import { resendEmailVerification, changeCurrentPassword, getCurrentUser, resetForgotPassword, forgotPasswordRequest, refreshAccessToken, verifyEmail, login, logoutUser, registerUser } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js"
 import { 
     userLoginValidator, 
-    userRegisterValidator
+    userRegisterValidator,
+    userChangeCurrentPasswordValidator,
+    userForgotPasswordValidator,
+    userResetForgotPasswordValidator
  } from "../validators/index.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // userRegisterValidator() ---> this will process the errors -- validate collects error and complete the proccess
+// Unsecure routes
 router.route("/register").post(userRegisterValidator(), validate, registerUser)
 router.route("/login").post(userLoginValidator(), validate, login)
+router.route("/verify-email/:verificationToken").get(verifyEmail)
+router.route("/refres-token").post(refreshAccessToken)
+router.route("/forgot-password").post(userForgotPasswordValidator(), validate, forgotPasswordRequest)
+router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(),validate , resetForgotPassword)
 
 // secure routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/change-password").post(verifyJWT,userChangeCurrentPasswordValidator(), validate ,changeCurrentPassword)
+router.route("/resend-email-verification").post(verifyJWT,resendEmailVerification)
+
 
 export default router
