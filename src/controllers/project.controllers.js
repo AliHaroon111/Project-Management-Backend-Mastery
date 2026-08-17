b6661454fbc6f2476c62b0dbfe7e0149fa0b1059
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-08-17 12:05:32 +0500

    feat(project): Add the getProjects controller and Write first Aggregation pipelines for getProjects

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -6,10 +6,67 @@ import ApiError from "../utils/api-error.js";
 import asyncHandler from "../utils/async-handler.js";
 import mongoose from "mongoose";
 import { userRoleEnum } from "../utils/constants.js";
+import { pipeline } from "nodemailer/lib/xoauth2/index.js";
 
 
-const getProject = asyncHandler( async (req, res) => {
-    // test
+const getProjects = asyncHandler( async (req, res) => {
+    const projects = await projectMember.aggregate([
+        {
+            $match: {
+                user: new mongoose.Types.ObjectId(req.user._id) // mean I'm 100% sure this is a mongoose ObjID
+            },
+        },
+        {
+            $lookup: {
+                from : "projects",
+                localField: "projects",
+                foreignField: "_id",
+                as: "projects",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "projectmember",
+                            localField: "_id",
+                            foreignField: "projects",
+                            as: "projectmembers"
+                        }
+                    },
+                    {
+                        $addFields: {
+                            members: {
+                                $size: "$projectmembers"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$projects"
+        },
+        {
+           $project: {
+                project: {
+                    _id: 1,
+                    name: 1,
+                    description: 1,
+                    members: 1,
+                    createdAt: 1,
+                    createdBy: 1
+                },
+                role: 1,
+                _id: 0
+           } 
+        }
+    ]);
+
+    return res
+        .status(200)
+        .json( new ApiResponse(
+            200,
+            projects,
+            "Projects fetched successfully"
+        ))
 });
 
 const getProjectById = asyncHandler( async (req, res) => {
@@ -72,7 +129,6 @@ const updateProject = asyncHandler( async (req, res) => {
         )
 });
 
-
 const deleteProject = asyncHandler( async (req, res) => {
     const {projectId} = req.params
 
@@ -94,16 +150,13 @@ const deleteProject = asyncHandler( async (req, res) => {
         )
 });
 
-
 const addMembersToProject = asyncHandler( async (req, res) => {
     // test
 });
 
-
 const getProjectMembers = asyncHandler( async (req, res) => {
     // test
 });
-
 
 const updateMemberRole = asyncHandler( async (req, res) => {
     // test
@@ -116,7 +169,7 @@ const deleteMember = asyncHandler( async (req, res) => {
 
 
 export {
-    getProject,
+    getProjects,
     getProjectById,
     createProject,
     updateProject,
