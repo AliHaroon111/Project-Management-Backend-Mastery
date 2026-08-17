7ffcc69f9add3ba40989bbffe657eb8a024e40c0
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-08-17 09:13:34 +0500

    feat(project): Create project and a projectMember as admin for the project itself

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -4,7 +4,8 @@ import {projectMember} from "../models/projectmember.models.js";
 import ApiResponse from "../utils/api-response.js";
 import ApiError from "../utils/api-error.js";
 import asyncHandler from "../utils/async-handler.js";
-
+import mongoose from "mongoose";
+import { userRoleEnum } from "../utils/constants.js";
 
 
 const getProject = asyncHandler( async (req, res) => {
@@ -16,7 +17,29 @@ const getProjectById = asyncHandler( async (req, res) => {
 });
 
 const createProject = asyncHandler( async (req, res) => {
-    // test
+    const {name, description} = req.body
+
+    const project = await Project.create({
+        name,
+        description,
+        createdBy: new mongoose.Types.ObjectId(req.user._id)
+    });
+    // we create a project - but this is our respnosibilty to create Admin for this project
+    await projectMember.create({
+        user: new mongoose.Types.ObjectId(req.user._id),
+        project: new mongoose.Types.ObjectId(project._id),
+        role: userRoleEnum.ADMIN
+    });
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(
+                201,
+                project,
+                "Project created successfuly"
+            )
+        )
 });
 
 const updateProject = asyncHandler( async (req, res) => {
