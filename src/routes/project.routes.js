1375d43796a2273a84de253cb6a963c313e44465
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-08-20 08:50:41 +0500

    🔓Lock🔒 the Learning RBAC: who should be able to get the project RoleBasedAccess

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -16,10 +16,24 @@ import {
     addMembersToProjectValidator
  } from "../validators/index.js"
 import { verifyJWT, validateProjecPermission } from "../middlewares/auth.middleware.js";
+import { AvailableUserRole, userRoleEnum } from "../utils/constants.js";
 
 const router = Router();
 router.use(verifyJWT)
 
+router
+    .route("/")
+    .get(getProjects)
+    .post(createProjecValidator(), validate, createProject) // we have "/" and the get request also running and the post also
 
+    router
+    .route("/:projectId")
+    .get(validateProjecPermission(AvailableUserRole), getProjectById) // who should be able to get the project
+    .put(
+        validateProjecPermission([userRoleEnum.ADMIN, userRoleEnum.MEMBER]),
+        createProjecValidator(),
+        validate,
+        updateProject
+    )
 
 export default router
